596eef44ddcf9281fd26a0c93243c7530ffa55c1
Saurav <sp.gharti@gmail.com>
2019-01-08 19:11:47 +0000

    add package django-scheduler

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'django-scheduler', 'url': 'http://pypi.python.org/pypi/django-scheduler/0.8.8'},
 	{'name':'pinax-notifications', 'url': 'http://pypi.python.org/pypi/pinax-notifications/5.0.0'},
 	{'name':'twilio', 'url': 'http://pypi.python.org/pypi/twilio/6.10.0'},
 	{'name':'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
